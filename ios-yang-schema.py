06816c7e75c0fd4d5fcf2d3effb72521d8173af7
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-10-30 23:06:26 +0700

    feat: get interfaces schema

--- ios-yang-schema.py
+++ ios-yang-schema.py
@@ -9,7 +9,7 @@ conn = manager.connect(host=host, port=port,
                     username=username, password=password, 
                     hostkey_verify=False, device_params={'name': 'csr'}, allow_agent=False, look_for_keys=False)
 
-connSchema = conn.get_schema('Cisco-IOS-XE-native')
+connSchema = conn.get_schema('ietf-interfaces')
 print(connSchema)
 
 conn.close_session()

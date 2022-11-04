e29d6628037fb4f1317fed7e618a5bd56544974e
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-11-04 16:04:15 +0700

    feat: parse output to xml format

--- netconf/ios-xml-get-config.py
+++ netconf/ios-xml-get-config.py
@@ -1,14 +1,18 @@
 from ncclient import manager
+import xml.dom.minidom
 
 host = "sandbox-iosxe-latest-1.cisco.com"
 username = "developer"
 password = "C1sco12345"
 port = 830
 
+
 conn = manager.connect(host=host, port=port, 
                     username=username, password=password, 
                     hostkey_verify=False, device_params={'name': 'csr'}, allow_agent=False, look_for_keys=False)
 
-print(conn.get_config('running'))
+output = (conn.get_config('running'))
+
+print(xml.dom.minidom.parseString(output.xml).toprettyxml())
 
 conn.close_session()
